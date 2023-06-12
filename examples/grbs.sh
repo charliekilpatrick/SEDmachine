7672199c172112957af22dbda9bd407bd858de8d
Charlie Kilpatrick <ckilpatrick@gmail.com>
2023-06-12 10:23:08 -0500

    Adding example scripts to document how models were generated

---
+++ examples/grbs.sh
@@ -0,0 +1,3 @@
+python sedmachine.py --models grb --instruments swift_uvot,sdss,ps1,ukirt,johnson --theta-obs 17
+python sedmachine.py --models grb --instruments swift_uvot,sdss,ps1,ukirt,johnson --theta-obs 22.5
+python sedmachine.py --models grb --instruments swift_uvot,sdss,ps1,ukirt,johnson
